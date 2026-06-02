b4132432e59b9278ba16598712940143901c7a95
juuhjuw <anajuliasprado16@gmail.com>
2026-06-02 16:06:54 -0300

    Estrutura inicial Node.js e Express

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
